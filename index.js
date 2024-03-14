69cffa4a54b69e1306109abe556ee228ab32915a
maooz4426@gmail.com <maooz4426@gmail.com>
2024-03-15 05:29:57 +0900

    first

---
+++ index.js
@@ -0,0 +1,39 @@
+const { Client, GatewayIntentBits, Partials } = require('discord.js'); //discord.js から読み込む
+require("dotenv").config();
+
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildBans,
+		GatewayIntentBits.GuildEmojisAndStickers,
+		GatewayIntentBits.GuildIntegrations,
+		GatewayIntentBits.GuildWebhooks,
+		GatewayIntentBits.GuildInvites,
+		GatewayIntentBits.GuildVoiceStates,
+		GatewayIntentBits.GuildPresences,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.GuildMessageReactions,
+		GatewayIntentBits.GuildMessageTyping,
+		GatewayIntentBits.DirectMessages,
+		GatewayIntentBits.DirectMessageReactions,
+		GatewayIntentBits.DirectMessageTyping,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildScheduledEvents,
+	],
+	partials: [
+		Partials.User,
+		Partials.Channel,
+		Partials.GuildMember,
+		Partials.Message,
+		Partials.Reaction,
+		Partials.GuildScheduledEvent,
+		Partials.ThreadMember,
+	],
+}); //clientインスタンスを作成する
+
+client.once('ready', () => { //ここにボットが起動した際のコードを書く(一度のみ実行)
+	console.log('起動完了'); //黒い画面(コンソール)に「起動完了」と表示させる
+});
+
+client.login(process.env.TOKEN); //ログインする
