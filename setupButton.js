6e74ac66adfa6330ed9eb3708dc3ca5cbd046ebc
maooz4426 <maooz4426@gmail.com>
2024-03-26 16:03:45 +0900

    情報変更ボタン追加

--- setupButton.js
+++ setupButton.js
@@ -1,17 +1,23 @@
 const {ButtonBuilder, ButtonStyle, ActionRowBuilder} = require('discord.js');
 
 async function setupButton(channel){
-    const button = new ButtonBuilder()
-        .setCustomId('openModal')
+    const admissionButton = new ButtonBuilder()
+        .setCustomId('openAdmissionModal')
         .setLabel('入会届')
         .setStyle(ButtonStyle.Primary);
 
-        const row = new ActionRowBuilder().addComponents(button);
+    
+    const changeButton = new ButtonBuilder()
+        .setCustomId('openChangeModal')
+        .setLabel('情報変更届')
+        .setStyle(ButtonStyle.Success);
+    
+    const row = new ActionRowBuilder().addComponents(admissionButton,changeButton);
 
-        await channel.send({
-            content: '入会届のフォームを開くには下のボタンをクリックしてください。',
-            components: [row]
-        });
+    await channel.send({
+        content: '入会届のフォームを開くには下のボタンをクリックしてください。',
+        components: [row]
+    });
     }
 
     module.exports = {setupButton};
